97d9767c34b509009a0d3cd779dcf548c656f539
Aleksandr Solovev <zuxelusa@ya.ru>
2022-08-16 22:20:40 +0300

    Example 006 Base

---
+++ Examples/Example006_Base/Program.cs
@@ -0,0 +1,20 @@
+﻿int a = 1;
+int b = 2;
+int c = 8;
+int d = 4;
+int e = 6;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("Максумум равен: ");
+Console.WriteLine(max);
+
+
+
+
