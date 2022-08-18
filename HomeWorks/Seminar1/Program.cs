8afb04f1b8183379062ba266f8d6fde54a0cefaa
Aleksandr Solovev <zuxelusa@ya.ru>
2022-08-18 20:52:19 +0300

    Quad calculated

--- HomeWorks/Seminar1/Program.cs
+++ HomeWorks/Seminar1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//int number = 5;
+
+//Console.WriteLine("My number is " + number + " and it's good!");
+
+//Console.WriteLine($"My number is {number} and it's good!");
+
+Console.Write("Input integer number: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int kvadrat = number * number;
+
+Console.WriteLine("Quad of " + number + " is " + kvadrat);
+
+
