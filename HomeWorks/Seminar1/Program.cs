45b3fc3a70dade68d307c18734f076b7ce4cd8c1
Aleksandr Solovev <zuxelusa@ya.ru>
2022-08-18 21:35:40 +0300

    Three tasks added

--- HomeWorks/Seminar1/Program.cs
+++ HomeWorks/Seminar1/Program.cs
@@ -15,7 +15,7 @@ int number = Convert.ToInt32(Console.ReadLine());
 int kvadrat = number * number;
 Console.WriteLine("Quad of " + number + " is " + kvadrat);
 */
-
+/*
 //Task 2. программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
 
 
@@ -33,3 +33,28 @@ else
 {
     Console.WriteLine($"{num1} не является квадратом {num2}");
 }
+*/
+
+/*
+//Task 2. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.Write("Input positive integer number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = num * (-1);
+
+while (current <= num)
+{
+    Console.Write(current + " ");
+    current++;
+}
+*/
+
+//Task 3. программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+Console.Write("Input 3-digital integer number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int lastSymbol = num % 10;
+
+Console.Write("The last digit is " + lastSymbol);
