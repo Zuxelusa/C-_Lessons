a261b74cd7d7e7b01e866ed2a344a47dc83b0922
Aleksandr Solovev <zuxelusa@ya.ru>
2022-08-22 20:30:39 +0300

    Начат второй семинар

---
+++ Практикум2/Program.cs
@@ -0,0 +1,18 @@
+﻿int FindBiggerDigit (int number)
+{
+int ed = number % 10;
+int dec = number / 10;
+
+int result;
+
+if (ed > dec) result = ed;
+else result = dec;
+
+return result;
+};
+
+int RandNumber = new Random().Next (10,100);
+int biggerDigit = FindBiggerDigit(RandNumber);
+
+Console.WriteLine($"bigger digit of {RandNumber} is {biggerDigit}");
+
