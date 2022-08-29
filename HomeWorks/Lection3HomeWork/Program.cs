f405dd94699b1e14961f57cabd1cac2b8c00eff1
Aleksandr Solovev <zuxelusa@ya.ru>
2022-08-29 20:04:22 +0300

    Task after lection 3 ready

---
+++ HomeWorks/Lection3HomeWork/Program.cs
@@ -0,0 +1,33 @@
+﻿//Написать программу которая выстроит массив по убыванию
+
+int[] array = { 5, 1, 7, 4, 9, 10, 18, 56, 37, 32, 1, 2, 3, 8 };
+
+
+void MassiveWrite(int[] arr)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+void SelectionSort(int[] arr)
+{
+    string str = string.Empty;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        int maxPosition = i;
+        for (int j = i + 1; j < arr.Length-1; j++)
+        {
+            if (arr[j] > arr[maxPosition]) maxPosition = j;
+        }
+        int temporary = arr[i];
+        arr[i] = arr[maxPosition];
+        arr[maxPosition] = temporary;
+    }
+}
+
+MassiveWrite(array);
+Console.WriteLine("");
+SelectionSort(array);
+MassiveWrite(array);
